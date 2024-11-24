63e52fe1ccf0c8fe91c01f8c8e307ee64f93f410
James W. Landry <jwlandry@alum.mit.edu>
2024-11-24 18:39:26 -0500

    added bitrate function

--- code/Load_Data.py
+++ code/Load_Data.py
@@ -1,45 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
-
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-
-# In[3]:
-
-
-traj = pd.read_csv("../data/fy25-adc-high-school-data.csv")
-
-
-# In[4]:
-
-
-traj
-
-
-# In[6]:
-
-
-traj.columns
-
-
-# In[41]:
+traj = pd.read_csv("../data/'FY25 ADC HS Data Updated.csv'")
 
 
 tplot = traj.plot(x=traj.columns[1],y=traj.columns[2])
 tplot.axis([-4e5,1.0e5,-2e5,3e5])
 tplot.axis('square')
-#tplot.axis.set_box_aspect(1)
-#ax.set_aspect('equal', adjustable='box')
-#tplot.show()
-
-
-# In[44]:
-
 
 ax = plt.figure().add_subplot(projection='3d')
 ax.plot(traj['Rx(km)[J2000-EARTH]'],traj['Ry(km)[J2000-EARTH]'],traj['Rz(km)[J2000-EARTH]'])
---
+++ code/data_funcs.py
@@ -0,0 +1,22 @@
+# Functions to calculate range and choose best deep space dish to use
+import numpy as np
+
+def find_best_dsn(sat_dsn_ranges,dsn_viz,dsn_antennas):
+    Pt = 10 # dBW
+    Gt = 9 # dBi
+    losses = 19.43 # dB
+    eta = 0.55 # efficiency gain
+    lambda_t = 0.136363636 # m, wavelength
+    kB = 228.6 dBW/K/Hz # Boltzmann Constant (in Decibel Watts per degree Kelvin per Hertz, dBW/K/Hz)
+    Ts = 22 # K
+
+    for (i, slant_ranges) in enumerate(sat_dsn_ranges):
+        for (j, slr) in enumerate(slant_ranges):
+            if (dsn_viz[i][j] > 0 and slr):
+                dB_part1 = Pt + Gt - losses + 10.0*np.log10(eta*(pi*dsn_antennas[i]/lambda_t)**2)
+                dB_part2 = 20.0*np.log10(4000*slr) + kB + 10.0*np.log10(Ts)
+                bitrate[i][j] = 10**((1/10.0)*(dB_part1 - dB_part2))/1000.0
+            else:
+                bitrate[i][j] = 0.0
+
+    return(bitrate)
