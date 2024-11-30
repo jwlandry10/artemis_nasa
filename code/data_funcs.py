74f250c2f7b90c2b4674043e9b28f95020d2229b
James W. Landry <jwlandry@alum.mit.edu>
2024-11-30 11:49:21 -0500

    Updated plots to calculate bitrates

--- code/data_funcs.py
+++ code/data_funcs.py
@@ -10,10 +10,11 @@ def find_best_dsn(sat_dsn_ranges,dsn_viz,dsn_antennas):
     kB = 228.6 # dBW/K/Hz Boltzmann Constant (in Decibel Watts per degree Kelvin per Hertz, dBW/K/Hz)
     Ts = 22 # K
 
+    bitrate = np.zeros((np.size(sat_dsn_ranges,0), np.size(sat_dsn_ranges,1)))
     for (i, slant_ranges) in enumerate(sat_dsn_ranges):
         for (j, slr) in enumerate(slant_ranges):
             if (dsn_viz[i][j] > 0 and slr):
-                dB_part1 = Pt + Gt - losses + 10.0*np.log10(eta*(pi*dsn_antennas[i]/lambda_t)**2)
+                dB_part1 = Pt + Gt - losses + 10.0*np.log10(eta*(np.pi*dsn_antennas[j]/lambda_t)**2)
                 dB_part2 = 20.0*np.log10(4000*slr) + kB + 10.0*np.log10(Ts)
                 bitrate[i][j] = 10**((1/10.0)*(dB_part1 - dB_part2))/1000.0
             else:
